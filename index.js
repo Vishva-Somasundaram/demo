c9295f16102147bc464a18942f75191f441b26fd
Vishva  Somasundaram <vishva26000@gmail.com>
2022-12-12 10:31:30 +0530

    name validation

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 var submit1 = document.getElementById("btn").addEventListener('click', storeInfo)
 
-var inputName = document.getElementById('name')
+var inputName = document.getElementById('name', validateName)
 var mobileNo = document.getElementById('mobile')
-document.getElementById('mobile').addEventListener('input',phoneValidation)
+document.getElementById('mobile').addEventListener('change', phoneValidation)
 
 var mailId = document.getElementById('mail')
-document.getElementById('mail').addEventListener('change',validateEmail)
+document.getElementById('mail').addEventListener('change', validateEmail)
 
 var subject = document.getElementById('subject')
 
 var password = document.getElementById('password')
-document.getElementById('password').addEventListener('change',validatePassword)
+document.getElementById('password').addEventListener('change', validatePassword)
 
 var msg = document.getElementById('message')
 
 let star = document.querySelectorAll('label')
 
-for(i=0; i<star.length; i++){
+for (i = 0; i < star.length; i++) {
     star[i].innerHTML += '<i class="fa-duotone fa-asterisk"></i>'
 }
 
-function storeInfo(e){
+function storeInfo(e) {
     e.preventDefault()
 
     let allDetail = {
@@ -34,31 +34,31 @@ function storeInfo(e){
     console.log(allDetail)
 
     let arr = []
-    if(inputName.value === ""){
+    if (inputName.value === "") {
         arr.push("enter name")
         inputName.style.border = '2px solid red';
     }
-    if(mobileNo.value === ""){
+    if (mobileNo.value === "") {
         arr.push("enter phone number")
         mobileNo.style.border = '2px solid red';
     }
-    if(mailId.value === ""){
+    if (mailId.value === "") {
         arr.push("enter mail id")
         mailId.style.border = '2px solid red';
     }
-    if(subject.value === ""){
+    if (subject.value === "") {
         arr.push("enter subject")
         subject.style.border = '2px solid red';
     }
-    if(password.value === ""){
+    if (password.value === "") {
         arr.push("enter password")
         password.style.border = '2px solid red';
     }
-    if(message.value === ""){
+    if (message.value == "") {
         arr.push("enter message")
         message.style.border = '2px solid red';
     }
-    
+
     console.log(arr)
     // var uname = localStorage.setItem("name", inputname)
     // var uno = localStorage.setItem("number", mobileNo)
@@ -78,21 +78,30 @@ function storeInfo(e){
     // console.log(msg.placeholder+':'+ msg.value)
 
 }
-function phoneValidation(){
+function phoneValidation() {
     var ten = document.getElementById('mobile').value
-    if(ten.length !== 10){
-        alert('enter 10 digit phone nmber')
+    if (ten.length !== 10) {
+        alert('enter 10 digit phone number')
     }
 }
-function validateEmail(){
+function validateEmail() {
     const mail = document.getElementById('mail').value
-    if(!mail.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)){
+    if (!mail.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) {
         alert('invalid mail id')
     }
 }
-function validatePassword(){
+function validatePassword() {
     const passwordvalue = document.getElementById('password').value
-    if(!passwordvalue.match("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})")){
+    if (!passwordvalue.match("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})")) {
         alert('example: Ul@2kfsd')
     }
+}
+function validateName() {
+    const uname = document.getElementById('name').value
+    if(!uname.match("^[a-zA-Z]+( [a-zA-Z]+)*$")){
+        alert('name must be in alphabet')
+    }
+
+    // const nameRegex = /^[a-zA-Z]+( [a-zA-Z]+)*$/;
+    // return nameRegex.test(uname);
 }
